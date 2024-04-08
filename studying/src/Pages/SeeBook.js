454c3bb09934276176fb6e1732f5446ef0e7a6bc
Fe Shiratsu <fehwebs@gmail.com>
2024-04-08 18:45:54 -0300

    SeeBook
    Augmented SeeBook page

--- studying/src/Pages/SeeBook.js
+++ studying/src/Pages/SeeBook.js
@@ -2,18 +2,25 @@
 import styles from '../Css/seebook.module.css'
 import image from '../Images/2 (1).jpg'
 import Cart from '../Images/carrinho-de-compras.png'
+import star from '../Images/star.png'
 function SeeBook(){
     return(
         <div className={styles.bookContainer}>
             <div className={styles.imgTile}>
-                <img src={image} alt=""/>
+                <img src={image} alt=""  />
             </div>
             <div className={styles.infoTile}>
                 <h1 className={styles.bookName}>Diary of banana</h1>
                 <div className={styles.rating}>
                     <p>Rating:</p>
+                    <img src={star} alt="" className={styles.star} />
+                    <img src={star} alt="" className={styles.star} />
+                    <img src={star} alt="" className={styles.star} />
+                    <img src={star} alt="" className={styles.star} />
+                    <img src={star} alt="" className={styles.star} />
                 </div>
-                <p className={styles.bookInfo}>Info about the book</p>
+                <p className={styles.bookInfo}>In the first book of the Diary of a Wimpy Kid series, #1 international bestselling author Jeff Kinney, introduces us to Greg Heffley: an unforgettable, unlikely hero that every family can relate to.
+Being a kid can really stink. And no one knows this better than Greg. He finds himself thrust into middle school, where undersized weaklings share the hallways with kids who are taller, meaner, and already shaving.</p>
 
                 <div className={styles.actionBar}>
                     <button>Buy now!</button>
